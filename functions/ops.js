2f0a063464a56cf07d2d5881a405079a0cef3b0f
nr-johnson <72578672+nr-johnson@users.noreply.github.com>
2022-08-18 16:00:45 -0500

    adding structure

--- functions/ops.js
+++ functions/ops.js
@@ -1,13 +1,27 @@
 const pug = require('pug')
+const fs = require('fs')
+const axios = require('axios')
+
+function getData() {
+
+}
 
 function siteOps() {
     return (req, res, next) => {
-        req.getData = route => {
+        // All data for GET requests are retrieved and compiled in this function.
+        req.getHTML = route => {
+            const pages = `${req.root}/views/pages`
             return new Promise((resolve, reject) => {
-                resolve(`Hello ${route}`)
+
+                if(!fs.existsSync(`${pages}${route}.pug`)) reject({err: 404})
+                try {
+                    const html = pug.renderFile(`${pages}${route}.pug`)
+                    resolve(html)
+                } catch(err) {
+                    reject(err)
+                }
             })
         }
-
         next()
     }
 }
--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const pkg = require('./package.json')
 const bodyParser = require('body-parser')
 const path = require('path')
 const ops = require('./functions/ops')
+const url = require('url')
 
 const app = express()
+
+app.use((req, res, next) => {
+    req.root = __dirname
+    next()
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,15 +23,32 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(ops.siteOps())
 
-app.get(['/', '/:path'], async (req, res) => {
-    const path = req.params.path || '/'
-    const data = await req.getData(path)
-    console.log(data)
+app.get('*', async (req, res) => {
+    const parsedUrl = url.parse(req.url)
+    let route = parsedUrl.pathname
+    if(route == '/') route = '/home'
+    
+    req.getHTML(route).then(html => {
+        if(req.query.data) { 
+            res.json({
+                html: html
+            })
+        } else {
+            const loc = route.split('/')
+            
+            res.render('main', {
+                title: 'NRJohnson | ' + (loc[loc.length - 1].substring(0,1).toLocaleUpperCase() + loc[loc.length - 1].substring(1)),
+                html: html
+            })
+        }
+    }).catch(err => {
+        res.status(500).json(err)
+    })
 })
 
 
 // Server ---
-const PORT = process.env.PORT || 8500
+const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 
 server.listen(PORT, () => { console.log(`${pkg.name} running on port ${PORT}`) })
